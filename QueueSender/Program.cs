fadec7e712173da01b2f84eab2a1186d11b7b686
Pierre Nallet <pierrenallet@hotmail.com>
2018-10-10 15:15:02 -0400

    Add project files.

---
+++ QueueSender/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Threading.Tasks;
+
+namespace QueueSender
+{
+	class Program
+	{
+		const string connectionString = @"Endpoint=sb://mybuspierre.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=sQ53l4BXM8Xwxp3bcphLJTAGdcFOtE+LPB1RuKE8UQA=";
+		static void Main(string[] args)
+		{
+			RunAsync().Wait();
+		}
+
+		static async Task RunAsync()
+		{
+			QueueClient client = new QueueClient(connectionString, "myqueue");
+			while (true)
+			{
+				string s = Console.ReadLine();
+				if (string.IsNullOrEmpty(s))
+					return;
+
+				var m = new Message(System.Text.Encoding.UTF8.GetBytes(s));
+				await client.SendAsync(m);
+			}
+		}
+	}
+}
+
+
